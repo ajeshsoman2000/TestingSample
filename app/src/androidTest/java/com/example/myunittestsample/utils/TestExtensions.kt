e630f5b4b4b0b7a4352115f576d7cebbd2e922fc
ajeshsoman2000 <ajeshsoman2000@gmail.com>
2020-10-19 11:25:03 +0530

    Minor modifications in tests

--- app/src/androidTest/java/com/example/myunittestsample/utils/TestExtensions.kt
+++ app/src/androidTest/java/com/example/myunittestsample/utils/TestExtensions.kt
@@ -1,10 +1,6 @@
 package com.example.myunittestsample.utils
 
-import android.app.Activity
-import android.content.Context
 import android.view.View
-import android.view.inputmethod.InputMethodManager
-import android.widget.Toast
 import com.google.android.material.textfield.TextInputLayout
 import org.hamcrest.Description
 import org.hamcrest.TypeSafeMatcher
@@ -16,13 +12,3 @@ fun tilHasError(errorText: String) = object : TypeSafeMatcher<View>() {
         override fun describeTo(description: Description) {
         }
     }
-
-fun Activity.hideKeypad(view: View) {
-    val imm: InputMethodManager =
-        this.getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
-    imm.hideSoftInputFromWindow(view.windowToken, 0)
-}
-
-fun Context.showToast(message: String) {
-    Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
-}
