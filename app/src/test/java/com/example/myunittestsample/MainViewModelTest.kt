c17e6001cbbe65892f67374049e14c6d74cdf87f
ajeshsoman2000 <ajeshsoman2000@gmail.com>
2020-10-19 11:32:04 +0530

    Replaced deprecated AndroidJunit4 with new one

--- app/src/test/java/com/example/myunittestsample/MainViewModelTest.kt
+++ app/src/test/java/com/example/myunittestsample/MainViewModelTest.kt
@@ -1,7 +1,7 @@
 package com.example.myunittestsample
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
-import androidx.test.runner.AndroidJUnit4
+import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.example.myunittestsample.utils.getOrAwaitValue
 import org.junit.After
 import org.junit.Assert.assertEquals
